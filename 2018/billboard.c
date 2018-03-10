2adb33c34f27316248de771a1e50b949a82c9247
Serena Mao <seranamao2003@gmail.com>
2018-03-09 22:14:26 -0800

    add billboard.in

--- 2018/billboard.c
+++ 2018/billboard.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main () {
+  //opening/reading files
   FILE *fin, *fout;
   int c;
   fin = fopen("billboard.in", "r");
   fout = fopen("billboard.out", "w");
-  while ((c = getc(fin)) != EOF)
+  
+  
+  //while ((c = getc(fin)) != EOF)
    
-    putc (c, fout);
+    for (int i = 1; i < 9; i++)
+      if (i == 1)
+	c = getc(fin);
+     
+	printf("%i", c);
   
   if(fout == NULL) {
     printf("Error opening file to write\n");

d1c1bce525f1cedeeed4fe72f6dc3be836dc8d70
Chris Mao <mao@iMac27.local>
2018-03-12 20:21:44 -0700

    tempate

---
+++ 2018/usaco_template.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#ifdef DEBUG 
+#define DBG_PRINT(fmt, args...) fprintf(stderr,fmt, ##args)
+#else
+#define DBG_PRINT(fmt, args...) /* Do nothing */
+#endif
+
+int main() {
+    FILE * fpin, *fpout;
+    if ((fpin = fopen("billboard.in","r"))==NULL) {
+        fprintf(stderr,"Unable to open billboard.in for reading\n");
+        return 1;
+    }
+    if ((fpout = fopen("billboard.out","w"))==NULL) {
+        fprintf(stderr,"Unable to open billboard.out for writing\n");
+        return 1;
+    }
+    
+    
+
+    fclose(fpin);
+    fclose(fpout);
+    return 0;
+}

f869b82ad0a33ea2ad418d47cc17424fb10a520e
Serena Mao <seranamao2003@gmail.com>
2018-03-14 21:55:04 -0700

    add working program

--- 2018/lifeguards.c
+++ 2018/lifeguards.c
@@ -18,9 +18,12 @@ int main() {
     }
     
     int schedule[1000];
-    int totalLifeguards, i, j;
+    int timeCheck, timeCovered, totalLifeguards, i, j, k;
     fscanf(fpin, "%d", &totalLifeguards);
-
+    for (i = 0; i < 1000; i++){
+      schedule[i] = 0;
+    }
+    //assembling the array
     for (i = 0; i < totalLifeguards; i++){
       int start, end;
       fscanf(fpin, "%d %d", &start, &end);
@@ -28,9 +31,42 @@ int main() {
       
       for (j = 0; j < (end - start); j++){
 	schedule[start + j] += 1;
+	DBG_PRINT("schedule's %d element is %d\n", start + j, schedule[start+j]);
 
       }
     }
+    rewind(fpin);
+    fscanf(fpin, "%d", &totalLifeguards); //useless and gets first one out of way           
+    //checking the array one at a time
+    for (i = 0; i < totalLifeguards; i++){
+      int start, end;
+      timeCheck = 0;
+       //useless and gets first one out of way
+     
+      fscanf(fpin, "%d %d", &start, &end);
+      DBG_PRINT("did it rewind: %d %d\n", start, end);
+      for (j = 0; j <(end - start); j++){
+	schedule[start + j] -= 1;
+      }
+      for (k = 0; k < 1000; k++){
+	if (schedule[k] != 0){
+	  timeCheck += 1;
+	  DBG_PRINT("schedule[%d] is %d right now\n", k, schedule[k]);
+	  //DBG_PRINT("timeCheck is %d right now", timeCheck);
+	}
+      }
+      if (timeCheck >= timeCovered){
+	timeCovered = timeCheck;
+      }
+      for (j = 0; j <(end - start); j++){
+        schedule[start + j] += 1;
+      }
+    }
+
+    for (i = 0; i < 1000; i++){
+      //DBG_PRINT ("%d\n", schedule[i]);
+    }
+    fprintf(fpout, "%d", timeCovered);
     
     
 
